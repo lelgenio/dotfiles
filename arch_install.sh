7f4598256e23214cfe004691ba725f833d0d0562
lelgenio <lelgenio@disroot.org>
2019-11-24 18:50:32 -0300

    update installer

--- arch_install.sh
+++ arch_install.sh
@@ -88,7 +88,8 @@ VIDEO_DRIVER="i915"
 REMOVE_PKGS='TRUE'
 # PACKAGES{{{
     pkgs_base=''
-    pkgs_base+=' base linux-zen linux-firmware intel-ucode lvm2 networkmanager bluez cronie git'
+    pkgs_base+=' base linux-zen linux-firmware intel-ucode lvm2 '
+    pkgs_base+=' zsh networkmanager bluez cronie git'
     pkgs_base+=" $(pacman -Qqg base-devel)"
 
 
@@ -137,7 +138,7 @@ REMOVE_PKGS='TRUE'
     # Chat
     pkgs+=' discord telegram-desktop telegram-cli-git'
     # Gaming
-    pkgs+=' steam lutris gamemode lutris-wine-meta wine-mono winetricks'
+    pkgs+=' steam lutris gamemode lutris-wine-meta wine wine-mono winetricks'
     if [ "$VIDEO_DRIVER" == "i915" ];then
         pkgs+=' xf86-video-intel '
         pkgs+=' lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader'
@@ -294,6 +295,9 @@ configure() {
     local boot_dev="$DRIVE"1
     local lvm_dev="$DRIVE"2
 
+    echo 'Setting repos'
+    set_repos
+
     echo 'Setting hostname'
     set_hostname "$HOSTNAME"
 
@@ -347,6 +351,7 @@ configure() {
     echo 'Creating initial user'
     create_user "$USER_NAME" "$USER_PASSWORD"
 
+    pacman -Syu 
     echo 'setting up user'
     cp $0 /home/$USER_NAME/setup.sh
     chown $USER_NAME /home/$USER_NAME/setup.sh
@@ -357,6 +362,112 @@ configure() {
     update_pkgfile
 
     rm /setup.sh
+}
+#}}}
+# set_repos() {{{{
+set_repos() {
+    cat > /etc/pacman.conf <<EOF
+#
+# /etc/pacman.conf
+#
+# See the pacman.conf(5) manpage for option and repository directives
+
+#
+# GENERAL OPTIONS
+#
+[options]
+# The following paths are commented out with their default values listed.
+# If you wish to use different paths, uncomment and update the paths.
+#RootDir     = /
+#DBPath      = /var/lib/pacman/
+#CacheDir    = /var/cache/pacman/pkg/
+#LogFile     = /var/log/pacman.log
+#GPGDir      = /etc/pacman.d/gnupg/
+#HookDir     = /etc/pacman.d/hooks/
+HoldPkg     = pacman glibc
+#XferCommand = /usr/bin/curl -L -C - -f -o %o %u
+#XferCommand = /usr/bin/wget --passive-ftp -c -O %o %u
+#CleanMethod = KeepInstalled
+#UseDelta    = 0.7
+Architecture = auto
+
+# Pacman won't upgrade packages listed in IgnorePkg and members of IgnoreGroup
+#IgnorePkg   =
+#IgnoreGroup =
+
+#NoUpgrade   =
+#NoExtract   =
+
+# Misc options
+#UseSyslog
+Color
+#TotalDownload
+CheckSpace
+#VerbosePkgLists
+
+# By default, pacman accepts packages signed by keys that its local keyring
+# trusts (see pacman-key and its man page), as well as unsigned packages.
+SigLevel    = Required DatabaseOptional
+LocalFileSigLevel = Optional
+#RemoteFileSigLevel = Required
+
+# NOTE: You must run `pacman-key --init` before first using pacman; the local
+# keyring can then be populated with the keys of all official Arch Linux
+# packagers with `pacman-key --populate archlinux`.
+
+#
+# REPOSITORIES
+#   - can be defined here or included from another file
+#   - pacman will search repositories in the order defined here
+#   - local/custom mirrors can be added here or in separate files
+#   - repositories listed first will take precedence when packages
+#     have identical names, regardless of version number
+#   - URLs will have $repo replaced by the name of the current repo
+#   - URLs will have $arch replaced by the name of the architecture
+#
+# Repository entries are of the format:
+#       [repo-name]
+#       Server = ServerName
+#       Include = IncludePath
+#
+# The header [repo-name] is crucial - it must be present and
+# uncommented to enable the repo.
+#
+
+# The testing repositories are disabled by default. To enable, uncomment the
+# repo name header and Include lines. You can add preferred servers immediately
+# after the header, and they will be used before the default mirrors.
+
+#[testing]
+#Include = /etc/pacman.d/mirrorlist
+
+[core]
+Include = /etc/pacman.d/mirrorlist
+
+[extra]
+Include = /etc/pacman.d/mirrorlist
+
+#[community-testing]
+#Include = /etc/pacman.d/mirrorlist
+
+[community]
+Include = /etc/pacman.d/mirrorlist
+
+# If you want to run 32 bit applications on your x86_64 system,
+# enable the multilib repositories as required here.
+
+#[multilib-testing]
+#Include = /etc/pacman.d/mirrorlist
+
+[multilib]
+Include = /etc/pacman.d/mirrorlist
+
+# An example of a custom package repository.  See the pacman manpage for
+# tips on creating your own repositories.
+#[custom]
+#SigLevel = Optional TrustAll
+#Server = file:///home/custompkgs
+EOF
 }
 #}}}
 # set_hostname() {#{{{
@@ -802,7 +913,7 @@ create_user() {
     local name="$1"; shift
     local password="$1"; shift
 
-    useradd -m -s =$USER_SHELL -G adm,systemd-journal,wheel,rfkill,games,network,video,audio,optical,floppy,storage,scanner,power,adbusers,wireshark "$name"
+    useradd -m -s /usr/bin/$USER_SHELL -G adm,systemd-journal,wheel,rfkill,games,network,video,audio,optical,floppy,storage,scanner,power "$name"
     echo -en "$password\n$password" | passwd "$name"
 }
 #}}}
