76d83c680309fd0ff0bc0c63e5a17484f8e0d48f
lelgenio <lelgenio@disroot.org>
2020-09-23 16:14:18 -0300

    fix insert mode bind

--- dotfiles/fish.fish
+++ dotfiles/fish.fish
@@ -102,6 +102,9 @@ if test $fish_key_bindings = fish_vi_key_bindings
     bind -M visual {{@@ key.up    @@}} up-line
     bind           {{@@ key.right @@}} forward-char
     bind -M visual {{@@ key.right @@}} forward-char
+
+    bind -m insert {{@@ key.insertMode @@}} repaint-mode
+
 end
 #}}}
 # start window manager if using tty1 {{{
