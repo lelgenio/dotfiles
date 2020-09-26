4c98428d9336292614709659d133afb9d8f5e397
lelgenio <lelgenio@disroot.org>
2020-09-26 03:09:25 -0300

    fix dark background on websites

--- dotfiles/qutebrowser.py
+++ dotfiles/qutebrowser.py
@@ -301,7 +301,7 @@ c.colors.tabs.pinned.selected.even.bg = "{{@@ color.bg_light @@}}"
 c.colors.webpage.darkmode.enabled = False
 c.colors.webpage.darkmode.threshold.background = 256 // 2
 c.colors.webpage.darkmode.threshold.text = 256 // 2
-c.colors.webpage.bg = "{{@@ color.bg @@}}"
+# c.colors.webpage.bg = "{{@@ color.bg @@}}"
 # }}}
 # Fonts {{{
 # Font used in the completion widget.
