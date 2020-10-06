94232e23c4801091fe0ba90fa159281fa43f5aab
lelgenio <lelgenio@disroot.org>
2020-10-06 11:01:55 -0300

    remove fisher

--- dotfiles/fish.fish
+++ dotfiles/fish.fish
@@ -161,13 +161,6 @@ end
     end
 
 # }}}
-# Install fisher{{{
-    if not functions -q fisher
-        set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-        curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-        fish -c fisher
-    end
-#}}}
 # Prompt customization{{{
 
     # Fine, I'll do it myself
