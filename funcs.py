302240df65dc9f7bc77aa2442dd67264ed579952
lelgenio <lelgenio@disroot.org>
2020-10-05 21:09:07 -0300

    use a sorted PATH to avoid re-runs

--- funcs.py
+++ funcs.py
@@ -3,3 +3,14 @@ import os
 
 def parent_dir(path):
     return os.path.split(path)[0]
+
+
+def ordered_path():
+    PATH = os.environ['PATH']
+
+    newPATH = []
+    for i in PATH.split(":"):
+        if i not in newPATH:
+            newPATH.append(i)
+
+    return ':'.join(newPATH)
