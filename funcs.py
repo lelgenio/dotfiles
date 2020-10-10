7df4667af8324d0ff83021d87aa1e4b1f2dc5e3c
lelgenio <lelgenio@disroot.org>
2020-10-10 03:35:37 -0300

    add bling to waybar

--- funcs.py
+++ funcs.py
@@ -14,3 +14,20 @@ def ordered_path():
             newPATH.append(i)
 
     return ':'.join(newPATH)
+
+
+def hex2rgb(e):
+    assert e.startswith("#")
+    e = e.strip("#").lower()
+    assert len(e) == 6
+    for i in e:
+        assert (i in "0123456789abcdef")
+
+    def h2r(i):
+        return str(eval('0x{}'.format(i)))
+
+    r = e[0:2]
+    g = e[2:4]
+    b = e[4:6]
+
+    return ", ".join([h2r(i) for i in (r, g, b)])
