1822af7f97ecf44f683c0941e8af7630d28d0a1e
lelgenio <lelgenio@disroot.org>
2020-10-25 18:29:13 -0300

    fix background

--- dotfiles/fish.fish
+++ dotfiles/fish.fish
@@ -311,7 +311,7 @@ set fish_color_operator purple
 set fish_color_command green
 set fish_color_autosuggestion --dim default
 
-set fish_pager_color_selected_background --background=red
+set fish_pager_color_selected_background --background={{@@ color.bg_light @@}}
 set fish_pager_color_prefix default --bold
 set fish_pager_color_completion default
 set fish_pager_color_description yellow
