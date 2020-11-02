3cc0e1a1b75aba365e0ff95ba724fe4479918fe3
lelgenio <lelgenio@disroot.org>
2020-11-02 14:32:10 -0300

    qutebrowser: Use mp4 for downloads

--- dotfiles/qutebrowser.py
+++ dotfiles/qutebrowser.py
@@ -248,7 +248,7 @@ c.fonts.downloads = fonts.BIG_INTER
 # }}}
 # Bindings {{{
 # Bindings for normal mode
-config.bind(",d", "spawn --verbose youtube-dl {url}")
+config.bind(",d", "spawn --verbose youtube-dl -fbest[ext=mp4] {url}")
 config.bind(",m", "spawn --userscript view_in_mpv")
 config.bind(",r", "spawn --userscript readability")
 config.bind(";e", "hint links spawn deemix '{hint-url}'")
